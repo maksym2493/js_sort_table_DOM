e7d5c22d2e80f33d841ed85589a5f4a34b93865c
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-23 12:27:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function sortTableBody(tBody, column) {
+  const rows = [...tBody.rows];
+
+  const data = extractData(rows).sort((rowA, rowB) => {
+    return typeof rowA[column] === 'string'
+      ? rowA[column].localeCompare(rowB[column])
+      : rowA[column] - rowB[column];
+  });
+
+  for (let i = data.length - 1; i >= 0; i--) {
+    tBody.prepend(rows.find((r) => r.cells[0].textContent === data[i][0]));
+  }
+}
+
+function extractData(body) {
+  return body.map((row) => [
+    row.cells[0].textContent,
+    row.cells[1].textContent,
+    row.cells[2].textContent,
+    +row.cells[3].textContent.slice(1).replaceAll(',', '') || 0,
+  ]);
+}
+
+const table = document.querySelector('table');
+
+[...table.tHead.rows[0].cells].forEach((cell, i) => {
+  cell.addEventListener('click', () => {
+    sortTableBody(table.tBodies[0], i);
+  });
+});
